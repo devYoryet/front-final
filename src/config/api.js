dd20d6970062d7c0abee50ad06639d20f487ce76
yoryet <y.danoun@pharmatender.cl>
2025-07-12 21:20:09 -0400

    test vercel imprimir variables

--- src/config/api.js
+++ src/config/api.js
@@ -2,12 +2,14 @@
 import axios from 'axios';
 // const DEPLOYED=''
 const LOCALHOST=process.env.REACT_APP_API_URL || 'http://localhost:5000';
+console.log("log REACT_APP_API_URL",process.env.REACT_APP_API_URL);
 
 export const API_BASE_URL = LOCALHOST
 
 const api = axios.create({
   baseURL: API_BASE_URL,
 });
+console.log("api", api);
 
 api.defaults.headers.post['Content-Type'] = 'application/json';
 
